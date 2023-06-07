93bd2683570e4384472d75ce720b85c4d0b89c8a
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-06-07 22:40:12 +0330

    fix: stable code

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -3,17 +3,18 @@ from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ContextTypes
 from lang import loadStrings
 from utils.auth import auth
-
-
+from utils.db_cache import db_cache
+from cache.cache_session import set_position
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from methods.menu import MenuManager
 
-
-print(2)
+@db_cache
 @auth
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE, db):
 
-    tel_id = update.effective_chat.id 
-
+    chat_id = update.effective_chat.id 
+    set_position(chat_id, 'mainmenu_manager', db)
+    await MenuManager().manager(update, context)
     # options = ReplyKeyboardMarkup([
     #     [
     #         KeyboardButton(loadStrings.button.buy_vpn)
@@ -28,10 +29,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     #     ]
     #     ], resize_keyboard=True) 
     
-        
-            
-    options = InlineKeyboardMarkup([[InlineKeyboardButton('ddd', callback_data='service_ssh_{0}'.format(2))]])
     
-    await context.bot.send_message(chat_id= tel_id, text= loadStrings.text.start_message, reply_markup=options)
+            
+    # options = InlineKeyboardMarkup([[InlineKeyboardButton('ddd', callback_data='service_ssh_{0}'.format(2))]])
+    
+    # await context.bot.send_message(chat_id= tel_id, text= loadStrings.text.start_message, reply_markup=options)
 
 
