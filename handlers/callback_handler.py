6499ad1e68eda1a1775dfa7a3aad1bf791946ff1
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-06-07 22:38:38 +0330

    feat: add pointer

--- handlers/callback_handler.py
+++ handlers/callback_handler.py
@@ -2,21 +2,29 @@ from telegram.ext import ContextTypes
 from telegram import Update
 import requests
 from utils.auth import auth
+from lang import loadStrings
+from utils.db_cache import db_cache
+from methods.new_config import NewConfigManager
+from methods.menu import MenuManager
 
+newConfigManager = NewConfigManager()
+mainmenu = MenuManager()
 
-
-async def buy_service(update: Update, context: ContextTypes.DEFAULT_TYPE):
+@db_cache
+async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, db):
     
     query = update.callback_query
-    _, service_type, interface_id = query.data.split('_')
-
-    requests.post()
-
-@auth
-async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
-    print('-------------')
-    print(context._chat_id)
+    # Acknowledge the button click and send a response
+    await query.answer()
+
+    callback_poitner = {
+        'newconfig' : lambda : newConfigManager.manager(update, context),
+        'mainmenu' : lambda : mainmenu.manager(update, context)
+    }
+
+    if query.data.split('_')[0] in callback_poitner:
+        await callback_poitner[query.data.split('_')[0]]()
     # query = update.callback_query
     # tel_id = query.message.chat.id
 
