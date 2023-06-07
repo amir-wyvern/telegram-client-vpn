add04c7fcfb6d227ebbae19f5824b6e59fb9a618
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-06-07 22:41:34 +0330

    fix: fix check session

--- utils/auth.py
+++ utils/auth.py
@@ -1,23 +1,22 @@
-from cache.cache_session import get_session
-from cache.database import get_redis_cache
-
+from cache.cache_session import get_session, set_position
+from methods.login import LoginManager
 
 def auth(func):
 
     async def wrapper(*args, **kwargs):
         
-        cache_db = get_redis_cache().__next__()
-        session = get_session(args[1]._chat_id, cache_db)
+        if kwargs.get('db', None) is None:
+            raise 'db decorator is not used'
         
+        session = get_session(args[1]._chat_id, kwargs['db'])
+        print('session: ', session)
         if session is None:
+            set_position(args[1]._chat_id, 'login_manager', kwargs['db'])
+            await LoginManager().manager(*args)
+            return
 
-
-        
-        kwargs['session'] = None
-        print("Before function execution")
-        # get_redis_cache()
         result = await func(*args, **kwargs)
-        print("After function execution")
+        print('res:', result)
         return result
     
     return wrapper
