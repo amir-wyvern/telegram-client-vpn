f0d504d24b6e7dfce8267f81831afc0e0df65021
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-06-16 15:54:33 +0330

    feat: add new handlers for block,unblock,status feature

--- handlers/message_handler.py
+++ handlers/message_handler.py
@@ -1,3 +1,4 @@
+
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ContextTypes
 from telegram import Update
@@ -6,13 +7,21 @@ from methods.login import LoginManager
 from methods.manage_users import ManageUsersManager
 from methods.update_expire import UpdateExpireConfigManager
 from methods.renew_config import RenewConfigManager
+from methods.user_status import UserStatusManager
+from methods.block_user import BlockUserManager
+from methods.unblock_user import UnBlockUserManager
+
 from utils.db_cache import db_cache
 from cache.cache_session import get_position
+
 
 loginManager = LoginManager()
 manageUsers = ManageUsersManager()
 updateExpire = UpdateExpireConfigManager()
 renewConfig = RenewConfigManager()
+userStatus = UserStatusManager()
+blockUser = BlockUserManager()
+unBlockUser = UnBlockUserManager()
 
 @db_cache
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, db):
@@ -24,7 +33,10 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, db
         'login': lambda : loginManager.manager(update, context),
         'manageusers': lambda: manageUsers.manager(update, context, edit= False),
         'updateexpire': lambda: updateExpire.manager(update, context, edit= False),
-        'renewconfig': lambda: renewConfig.manager(update, context, edit= False)
+        'renewconfig': lambda: renewConfig.manager(update, context, edit= False),
+        'userstatus': lambda: userStatus.manager(update, context, edit=False),
+        'blockuser': lambda: blockUser.manager(update, context, edit=False),
+        'unblockuser': lambda: unBlockUser.manager(update, context, edit=False)
     }
     
     if pos.split('_')[0] in messages_pointer:
