ad06878bab97d84298b50dc0334526e159566610
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-06-16 15:53:53 +0330

    feat: add handler for block,unblock,status feature

--- handlers/callback_handler.py
+++ handlers/callback_handler.py
@@ -9,6 +9,10 @@ from methods.menu import MenuManager
 from methods.manage_users import ManageUsersManager
 from methods.update_expire import UpdateExpireConfigManager
 from methods.renew_config import RenewConfigManager
+from methods.user_status import UserStatusManager
+from methods.block_user import BlockUserManager
+from methods.unblock_user import UnBlockUserManager
+from methods.help import HelpManager
 
 
 newConfigManager = NewConfigManager()
@@ -16,6 +20,10 @@ mainmenu = MenuManager()
 manageUsers = ManageUsersManager()
 updateExpire = UpdateExpireConfigManager()
 renewConfig = RenewConfigManager()
+userStatus = UserStatusManager()
+blockUser = BlockUserManager()
+unblockUser = UnBlockUserManager()
+help = HelpManager()
 
 @db_cache
 async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, db):
@@ -29,9 +37,13 @@ async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, d
         'mainmenu' : lambda : mainmenu.manager(update, context, edit=True),
         'manageusers': lambda: manageUsers.manager(update, context, edit=True),
         'updateexpire': lambda: updateExpire.manager(update, context, edit=True),
-        'renewconfig': lambda: renewConfig.manager(update, context, edit=True)
+        'renewconfig': lambda: renewConfig.manager(update, context, edit=True),
+        'userstatus': lambda: userStatus.manager(update, context, edit=True),
+        'blockuser': lambda: blockUser.manager(update, context, edit=True),
+        'unblockuser': lambda: unblockUser.manager(update, context, edit=True),
+        'help': lambda: help.manager(update, context, edit=True),
     }
-
+    print(query.data)
     if query.data.split('_')[0] in callback_poitner:
         await callback_poitner[query.data.split('_')[0]]()
 
