fd741f43efc9d255506bf67885566d1127446dd8
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-07-26 12:05:57 +0330

    style: clean up

--- methods/new_config.py
+++ methods/new_config.py
@@ -265,6 +265,9 @@ class NewConfigManager:
 
         for line in sp_old_text[1:]:
             main_line, strike_line = line.split(' ')
+            if main_line == 'username:':
+                new_text.append(line)
+                continue
             new_text.append(f'{main_line} <s>{strike_line}</s>')
 
         join_text=  '\n'.join(new_text).strip()
