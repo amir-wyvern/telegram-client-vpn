07327c7ae04e70761bd79e99913da4e9b05fd105
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-11-01 13:04:55 +0330

    fix: fix bug

--- utils/auth.py
+++ utils/auth.py
@@ -4,23 +4,28 @@ from methods.login import LoginManager
 def auth(func):
 
     async def wrapper(*args, **kwargs):
-        
+
         if kwargs.get('db', None) is None:
             raise 'db decorator is not used'
-        
+
+
         if hasattr(args[1], '_chat_id'):
             chat_id = args[1]._chat_id
+            _args = args
         else:
             chat_id = args[1].effective_chat.id
+            _args = args[1:]
 
         session = get_session(chat_id, kwargs['db'])
         if session is None:
+
             set_position(chat_id, 'login_manager', kwargs['db'])
-            await LoginManager().manager(*args)
+
+            await LoginManager().manager(*_args, db=kwargs['db'])
             return
 
 
         result = await func(*args, **kwargs)
         return result
-    
+
     return wrapper
